a87330509ff5b869bfe6211478f09307137b7e40
KiruMinato <kiruminato@gmail.com>
2025-10-06 18:42:25 +0300

    Мой код тг бота

---
+++ main.py
@@ -0,0 +1,97 @@
+
+
+import telebot
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from telebot import types
+from telebot.types import InlineKeyboardMarkup
+
+
+bot = telebot.TeleBot('')
+
+name = None
+
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="sk1726ks",
+                                  host="localhost",
+                                  port="1726")
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f'Hello, {message.from_user.username}')
+@bot.message_handler(commands=['start'])
+def info(message):
+    connection = psycopg2.connect(user="postgres",
+                                  password="sk1726ks",
+                                  host="localhost",
+                                  port="1726",
+                                  database="EngTGBot")
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS USERS (id serial primary key, name varchar(50), tg_name varchar(50) UNIQUE, role text,)')
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    bot.send_message(message.chat.id, f"Здравствуйте, {message.from_user.first_name}!\n"
+                                      f" Сейчас я вас зарегистрирую.\n"
+                                      f"Введите ваше имя:   ")
+    bot.register_next_step_handler(message, user_name)
+
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    connection = psycopg2.connect(
+       user="postgres",
+       password="sk1726ks",
+       host="localhost",
+       port="1726",
+       database="EngTGBot")
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO users (name, tg_name) VALUES (%s, %s)', (name, message.from_user.username))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    markup = types.InlineKeyboardMarkup()
+    btn=types.InlineKeyboardButton('Список пользователей', callback_data='users')
+    markup.add(btn)
+    bot.send_message(message.chat.id, 'Регистрация прошла успешно!', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    connection = psycopg2.connect(user="postgres",
+                                  password="sk1726ks",
+                                  host="localhost",
+                                  port="1726",
+                                  database="EngTGBot")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}| TG: {el[2]}| Role: {el[3]}\n'
+    cursor.close()
+    connection.close()
+    bot.send_message(call.message.chat.id, info)
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
+    # markup = types.InlineKeyboardMarkup()
+    # btn = types.InlineKeyboardButton("Ученик", callback_data="student")
+    # btn2 = types.InlineKeyboardButton("Учитель", callback_data="teacher")
+    # markup.add(btn)
+    # markup.add(btn2) , reply_markup=markup
+    # @bot.callback_query_handler(func=lambda call: True)
+    # def callback(call):
+    #     if call.data == "student":
+    #         bot.answer_callback_query(call.id, "Вы выбрали Ученик!")
+    #         bot.send_message(call.message.chat.id, "Отлично, теперь вы ученик!")
+    #     if call.data == "teacher":
+    #         bot.answer_callback_query(call.id, "Вы выбрали Учитель!")
+    #         bot.send_message(call.message.chat.id, "Отлично, теперь вы учитель!")
